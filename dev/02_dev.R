ff6cb86593066ecd606e3bba6b069dab27e13204
kristian-bak <kristianbak@live.com>
2022-03-22 10:22:39 +0100

    Clean up

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -1,20 +1,10 @@
-# Building a Prod-Ready, Robust Shiny Application.
-# 
-# README: each step of the dev files is optional, and you don't have to 
-# fill every dev scripts before getting started. 
-# 01_start.R should be filled at start. 
-# 02_dev.R should be used to keep track of your development during the project.
-# 03_deploy.R should be used once you need to deploy your app.
-# 
-# 
-###################################
-#### CURRENT FILE: DEV SCRIPT #####
-###################################
 
-# Engineering
+## Load all functions
+devtools::load_all()
 
-## Dependencies ----
-## Add one line by package you want to add as dependency
+## Bump version number
+usethis::use_version()
+
 usethis::use_package("shinydashboard")
 usethis::use_package("htmltools")
 usethis::use_package("DT")
@@ -30,45 +20,19 @@ usethis::use_package("purrr")
 
 remotes::install_github(repo = "https://github.com/kristian-bak/kb.yahoo/")
 
-
-## Install new version of kb.yahoo
-devtools::install_git("https://github.com/kristian-bak/kb.yahoo/")
-
-## Add modules ----
-## Create a module infrastructure in R/
 golem::add_module(name = "overview") # Name of the module
 golem::add_module(name = "name_of_module2") # Name of the module
 
-## Add helper functions ----
-## Creates fct_* and utils_*
 golem::add_fct( "helpers" ) 
 golem::add_utils( "helpers" )
-
-## Add pipe operator
-usethis::use_pipe()
-
-## Bump version number
-usethis::use_version()
-
-## External resources
-## Creates .js and .css files at inst/app/www
-golem::add_js_file( "script" )
-golem::add_js_handler( "handlers" )
-golem::add_css_file( "custom" )
-
-## Add internal datasets ----
-## If you have data in your package
-usethis::use_data_raw( name = "my_dataset", open = FALSE ) 
 
 ## Tests ----
 ## Add one line by test you want to create
 usethis::use_test( "app" )
 
-# Documentation
+## Add pipe operator
+usethis::use_pipe()
 
-## Vignette ----
-usethis::use_vignette("shinyStocks")
-devtools::build_vignettes()
 
 ## Code Coverage----
 ## Set the code coverage service ("codecov" or "coveralls")
